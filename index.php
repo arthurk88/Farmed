7c1e1e5a25695bc47494f75508c2a08eb1afd544
Arthur Santana <arthur_k88@hotmail.com>
2018-12-26 16:01:21 -0200

    Fundo com troca de Imagens no login

--- index.php
+++ index.php
@@ -6,32 +6,35 @@
 	<link rel="stylesheet" type="text/css" href="_css/mode-1.css">
 	<script type="text/javascript" src="../_js/cycle.js"></script>
 	<script type="text/javascript" src="../_js/jquery.js"></script>
-
-
-<script>
-var id = 0;
-var imgs = new Array();
-imgs[0] = "http://garotasquecurtemanimes.com.br/wp-content/uploads/2017/03/%E7%AC%AC%E4%B8%80%E6%AC%A1%E7%9A%84%E8%BE%A3%E5%A6%B9-800x400.jpg";
-imgs[1] = "http://garotasquecurtemanimes.com.br/wp-content/uploads/2017/03/%E7%AC%AC%E4%B8%80%E6%AC%A1%E7%9A%84%E8%BE%A3%E5%A6%B9-800x400.jpg";
-imgs[2] = "http://garotasquecurtemanimes.com.br/wp-content/uploads/2017/03/%E7%AC%AC%E4%B8%80%E6%AC%A1%E7%9A%84%E8%BE%A3%E5%A6%B9-800x400.jpg";
-imgs[3] = "http://garotasquecurtemanimes.com.br/wp-content/uploads/2017/03/%E7%AC%AC%E4%B8%80%E6%AC%A1%E7%9A%84%E8%BE%A3%E5%A6%B9-800x400.jpg";
-//Aqui apenas adicione mais imagens
-function troca(){
-	if (id<imgs.length-1){
-	id++;	
-	}else{
-		id=0
-	}
-	$("#box").fadeOut(500);
-	setTimeout("$('#box').html('<img src=\""+imgs[id]+"\" width=\"100%\" height=\"100%\" />');$('#box').fadeIn(500);",500);
+<script language="Javascript">
+function skin1()
+{
+document.bgColor='red';
 }
-var segundos = 3; //Segundos entre cada imagem
-setInterval("troca();",segundos*1s00);
+function skin2()
+{
+document.bgColor='green';
+}
+function skin3()
+{
+document.bgColor='yellow';
+}
+function skin4()
+{
+document.bgColor='#FFFFCC';
+}
+
 </script>
+
 </head>
 
 <body>
-<div id="box"><img src="img1.jpg" width="100%" height="100%" /></div>
+<form name="cores">
+<input type="radio" name="campo" onclick="skin1();">Fundo vermelho<br>
+<input type="radio" name="campo" onclick="skin2();">Fundo verde<br>
+<input type="radio" name="campo" onclick="skin3();">Fundo amarelo<br>
+<input type="radio" name="campo" onclick="skin4();">Fundo amarelo<br>
+</form>
 
 <?php
 include "obj-menu.php";
